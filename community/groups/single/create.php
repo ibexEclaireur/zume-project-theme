e628ca882d7e5daae83126e2727d7d10c6766721
corsac <corsac@hushmail.com>
2017-06-09 13:35:10 +0100

    Invite Group page on the group settings

--- community/groups/single/create.php
+++ community/groups/single/create.php
@@ -1,7 +1,6 @@
 <?php
 
 
-//@todo set default to prvita
 function bp_remove_group_step_settings($array) {
 
 	$array = array(
@@ -15,10 +14,11 @@ function bp_remove_group_step_settings($array) {
 }
 add_filter ('groups_create_group_steps', 'bp_remove_group_step_settings', 10, 1);
 
-/**
- * The bp_is_active( 'groups' ) check is recommended, to prevent problems
- * during upgrade or when the Groups component is disabled
- */
+
+
+if ( !defined( 'BP_INVITE_ANYONE_SLUG' ) )
+	define( 'BP_INVITE_ANYONE_SLUG', 'invite-anyone' );
+
 if ( bp_is_active( 'groups' ) ) :
 
 
@@ -27,41 +27,36 @@ if ( bp_is_active( 'groups' ) ) :
 		 * Your __construct() method will contain configuration options for
 		 * your extension, and will pass them to parent::init()
 		 */
+
 		function __construct() {
 			$args = array(
 				'slug' => 'group_invite_by_url',
 				'name' => 'Invite By URL',
-				'screens' => array(
-					'create' => array(
-						'position' => 10,
-					),
-					'edit' => array(
-						'enabled' => false,
-					),
-					'admin' => array(
-						'enabled' => false,
-					)
-				),
-//				'show_tab' => 'noone'
+        "visibility" => "private",
+        "show_tab"=> 'admin',
+        "access" => "admin",
+
+
 			);
 			parent::init( $args );
 		}
 
+		function get_invite_anyone_email_link($group_id){
+		  return bp_loggedin_user_domain() . BP_INVITE_ANYONE_SLUG . '/invite-new-members/group-invites/' . $group_id;
+    }
 
+    function display( $group_id = NULL ) {
+      $this->settings_screen($group_id);
+    }
 
 
 		function create_screen_save($group_id = NULL){
-		  global $bp;
-			update_option("testurl", $_POST);
-
 			if (isset($_POST["redirect_invite"]) && $_POST["redirect_invite"] == "yes"){
-				  bp_core_redirect( bp_get_group_permalink( $bp->groups->current_group ). "invite-anyone/");
+				  bp_core_redirect( $this->get_invite_anyone_email_link($group_id));
 			} else {
 				  bp_core_redirect( "/dashboard");
       		}
 		}
-
-
 
 		function create_screen($group_id = NULL){
 			global $bp;
@@ -69,30 +64,84 @@ if ( bp_is_active( 'groups' ) ) :
 
 		  $returned_data = !empty( $bp->invite_anyone->returned_data ) ? $bp->invite_anyone->returned_data : false;
 			  $returned_groups = array( 0 );
-	    if ( ! empty( $returned_data['groups'] ) ) {
-		    foreach( $returned_data['groups'] as $group_id ) {
-			    $returned_groups[] = $group_id;
-		    }
-	    }
+        if ( ! empty( $returned_data['groups'] ) ) {
+          foreach( $returned_data['groups'] as $group_id ) {
+            $returned_groups[] = $group_id;
+          }
+        }
         $returned_message = ! empty( $returned_data['message'] ) ? stripslashes( $returned_data['message'] ) : false;
 
-			  	    // Get the returned email message, if there is one
 
 
-        $group_name = "";
+        $token = groups_get_groupmeta($group_id, "group_token");
+        update_option("group_token_test", $token);
 
-
-        $info_url = get_site_url() . "/about/?zume-group-id=".$group_id;
-        $sign_up_url = get_site_url() . "/register?zume-group-id=".$group_id
+        $sign_up_url = get_site_url() . "/register/?group-id=".$group_id ."&zgt=" . $token
 
         ?>
 
         <h2>To Invite people you have 3 options</h2>
-          <p>Invite 3-11 friends, you have to have 4 people present to start the Zume Sessions.</p>
+        <p>Invite 3-11 friends, you have to have 4 people present to start the Zume Sessions.</p>
+        <ul>
+          <li>
+            <p><strong>Send this invite link to your group</strong></p>
+              <pre><a href="<?php echo $sign_up_url?>"><?php echo $sign_up_url?></a></pre></li>
+          </li>
+
+          <li>
+            <p><strong>Copy and send this Email template:</strong></p>
+            <p>
+
+            <pre style="white-space: pre-line;">
+              <?php echo esc_textarea( invite_anyone_invitation_message( $returned_message ) ) ?>
+            </pre>
+
+            </p>
+          </li>
+          <li>
+            <p><strong>Have the email come from Zume:</strong></p>
+            <p>
+              <input type="checkbox" name="redirect_invite" id="redirect_invite" value="yes"  />
+                Check this box if you would like the invite email to come from Zume.
+              You will be redirected after you click finish.
+            </p>
+          </li>
+        </ul>
+        <?php
+      }
+		}
+
+
+	  /**
+	   * settings_screen() is the catch-all method for displaying the content
+	   * of the edit, create, and Dashboard admin panels
+	   */
+	  function settings_screen( $group_id = NULL ) {
+	    global $bp;
+	    if ($group_id > 0){
+
+		    $returned_data = !empty( $bp->invite_anyone->returned_data ) ? $bp->invite_anyone->returned_data : false;
+		    $returned_groups = array( 0 );
+		    if ( ! empty( $returned_data['groups'] ) ) {
+			    foreach( $returned_data['groups'] as $group_id ) {
+				    $returned_groups[] = $group_id;
+			    }
+		    }
+		    $returned_message = ! empty( $returned_data['message'] ) ? stripslashes( $returned_data['message'] ) : false;
+
+
+
+		    $token = groups_get_groupmeta($group_id, "group_token");
+		    update_option("group_token_test", $token);
+
+		    $sign_up_url = get_site_url() . "/register/?group-id=".$group_id ."&zgt=" . $token
+
+		    ?>
+          <h3>Invite 3-11 friends, you have to have 4 people present to start the Zume Sessions.</h3>
           <ul>
             <li>
               <p><strong>Send this invite link to your group</strong></p>
-                <pre><a href="<?php echo $sign_up_url?>"><?php echo $sign_up_url?></a></pre></li>
+              <pre><a href="<?php echo $sign_up_url?>"><?php echo $sign_up_url?></a></pre></li>
             </li>
 
             <li>
@@ -100,23 +149,24 @@ if ( bp_is_active( 'groups' ) ) :
               <p>
 
               <pre style="white-space: pre-line;">
-                <?php echo esc_textarea( invite_anyone_invitation_message( $returned_message ) ) ?>
-              </pre>
+              <?php echo esc_textarea( invite_anyone_invitation_message( $returned_message ) ) ?>
+            </pre>
 
               </p>
             </li>
             <li>
               <p><strong>Have the email come from Zume:</strong></p>
               <p>
-                <input type="checkbox" name="redirect_invite" id="redirect_invite" value="yes"  />
-                  Check this box if you would like the invite email to come from Zume.
+                Click <a href="<?php echo $this->get_invite_anyone_email_link($group_id)?>">here</a> if you would like the invite email to come from Zume.
                 You will be redirected after you click finish.
               </p>
             </li>
           </ul>
-        <?php
-      }
-		}
+		    <?php
+	    }
+	  }
+
+
 	}
 
 	bp_register_group_extension( 'Invite_By_URL' );
