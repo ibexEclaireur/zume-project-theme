7ba22904379c677f3d564e74c23700dfd0142753
Burning Sun <burningsun@example.com>
2017-06-13 10:34:47 +0100

    Build files before launching browsersync
    This leads to less confusion IMHO.

--- gulpfile.js
+++ gulpfile.js
@@ -103,7 +103,7 @@ gulp.task('bower', function() {
 });
 
 // Browser-Sync watch files and inject changes
-gulp.task('browsersync', function() {
+gulp.task('browsersync', ['styles', 'site-js', 'foundation-js'], function() {
     // Watch files
     var files = [
     	'./assets/css/*.css',
