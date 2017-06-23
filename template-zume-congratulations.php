ff6ce2b6063625b14befb6720e65c59f099e3279
Burning Sun <burningsun@example.com>
2017-06-23 12:22:04 +0100

    Add buttons in congratulations pages
    The images were resized to be smaller.

---
+++ template-zume-congratulations.php
@@ -0,0 +1,56 @@
+<?php
+/*
+Template Name: Zúme Congratulations on completing
+*/
+
+get_header();
+
+?>
+
+<div class="row block">
+    <div class="step-title" style="text-transform: uppercase">Congratulations on completing Zúme training!</div>
+</div>
+
+<div class="row block">
+    <div class="activity-title"><span>WATCH</span></div>
+    <div class="activity-description">
+        You and your group are now ready to take leadership to a new level!
+        Here are a few more steps to help you KEEP growing!
+    </div>
+</div>
+
+<div class="row block">
+    <div class="small-12 small-centered medium-9 columns">
+        <script src="//fast.wistia.com/embed/medias/h3znainxm9.jsonp" async></script>
+        <script src="//fast.wistia.com/assets/external/E-v1.js" async></script>
+        <div class="wistia_embed wistia_async_h3znainxm9" >&nbsp;</div>
+    </div>
+</div>
+
+<div class="row block">
+    <div class="activity-title"><span>GROW</span></div>
+    <div class="activity-description">
+        <p style="text-transform: uppercase">Grow as a disciple by putting your faith to work</p>
+        <p>
+            Consider registering online for reminders, coaching resources, and to become connected
+            with others who are using the same sort of mininistry approach.
+            You can do this at ZumeProject.com.
+        </p>
+    </div>
+</div>
+
+<div class="row block">
+    <div class="activity-title"><span>ACT</span></div>
+    <div class="activity-description">
+        <div class="congratulations-more">
+            <button class="button"><div class="congratulations-icon congratulations-icon-learn-more"></div> <span>Learn More</span></button>
+            <button class="button"><div class="congratulations-icon congratulations-icon-invite"></div> <span>Invite my friends</span></button>
+            <button class="button"><div class="congratulations-icon congratulations-icon-coordinator"></div> <span>Become a county coordinator</span></button>
+            <button class="button"><div class="congratulations-icon congratulations-icon-map"></div> <span>Map my neighborhood</span></button>
+            <button class="button"><div class="congratulations-icon congratulations-icon-language"></div> <span>Fund translation of Zúme</span></button>
+            <button class="button"><div class="congratulations-icon congratulations-icon-contact-coach"></div> <span>Contact my coach</span></button>
+        </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>
