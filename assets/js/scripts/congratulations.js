f90097df0f9d092928cf720c022c89258ed4d456
Burning Sun <burningsun@example.com>
2017-06-23 16:08:49 +0100

    Delete template, instead put HTML in Session 10 Course page
    Also, some Javascript changes to get the "Invite friends" button to
    work.

--- assets/js/scripts/congratulations.js
+++ assets/js/scripts/congratulations.js
@@ -12,11 +12,20 @@ jQuery(function($) {
     }
   });
 
-
+  var group_permalink = $(".js-group-info").data("group-permalink");
+  if (group_permalink) {
+    var href = group_permalink + "group_invite_by_url/";
+    $(".js-congratulations-more-invite-button").append(
+      $("<p>").append(
+       $("<a>").addClass("button").attr("href", href).text("Invite friends")
+      )
+    );
+  }
 
   function isScrolledIntoView(el) {
     var rect = el.getBoundingClientRect();
     var isVisible = (rect.top >= 0) && (rect.bottom <= window.innerHeight);
     return isVisible;
   }
+
 });
