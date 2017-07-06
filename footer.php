cf14f1d50c7c066b7de1706ab7f60093db0fd4de
Burning Sun <burningsun@example.com>
2017-07-06 12:26:25 +0100

    Remove link old.zumeproject.com

--- footer.php
+++ footer.php
@@ -8,7 +8,6 @@
 						<div class="large-12 medium-12 columns">
 							<p class="source-org copyright">
 							    &copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.
-							    | <a href="https://old.zumeproject.com">old.zumeproject.com</a>
                                 <?php if( user_can( get_current_user_id(), 'manage_options')) { echo '| <a href="/wp-admin" > Admin Panel</a>'; } ?>
                             </p>
 						</div>
