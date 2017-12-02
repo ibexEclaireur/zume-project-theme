8cb7f05ab4a33de39f9fee5edf74f5f582b1538c
corsac <corsac@hushmail.com>
2017-12-02 16:10:59 +0100

    Add google table and chart to progress page

--- template-zume-progress.php
+++ template-zume-progress.php
@@ -15,8 +15,23 @@ get_header();
                 <img class="center" src="<?php echo esc_attr( get_stylesheet_directory_uri() ) . '/assets/images/'; ?>zume-translation-infographic.png" alt="" />
 
 
-                <h3>Groups locations</h3>
+                <h3>Groups locations in the US</h3>
                 <div id="group-markers" style="width: 900px; height: 600px;"></div>
+
+                <br>
+                <br>
+                <h3>Translation Progress</h3>
+                <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vS3rJ_shcus0nIrYWHzFpWY5F5UfiYWb4ql8kaNTtWDf5htAA8-KmiVwV49RYWIxZumfI6zVH5fY4ZG/pubhtml?gid=2023643321&amp;single=true&amp;widget=true&amp;headers=false"
+                width="50%" height="400px"
+                ></iframe>
+<br>
+                <br><br>
+                <h3>Progress for each language</h3>
+                <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vS3rJ_shcus0nIrYWHzFpWY5F5UfiYWb4ql8kaNTtWDf5htAA8-KmiVwV49RYWIxZumfI6zVH5fY4ZG/pubhtml?gid=0&amp;single=true&amp;widget=true&amp;headers=false"
+                width="100%"
+                height="900px"
+                ></iframe>
+
             </main>
         </div>
     </div>
