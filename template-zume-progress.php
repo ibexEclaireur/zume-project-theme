d48bbc8c25b2b7b3340d30d5fca0805752cab11c
corsac <corsac@hushmail.com>
2017-12-05 10:32:44 +0100

    Progress pages center everything

--- template-zume-progress.php
+++ template-zume-progress.php
@@ -10,26 +10,28 @@ get_header();
     <div id="content">
         <div id="inner-content" class="row">
 
-            <main id="main" class="large-12 medium-12 columns" role="main">
-
-                <img class="center" src="<?php echo esc_attr( get_stylesheet_directory_uri() ) . '/assets/images/'; ?>zume-translation-infographic.png" alt="" />
-
+            <main id="main" class="large-12 medium-12 columns" role="main" style="text-align: center">
 
                 <h3>Groups locations in the US</h3>
-                <div id="group-markers" style="width: 900px; height: 600px;"></div>
+                <div id="group-markers" style="width: 100%; height: 600px;"></div>
+
+                <a href="https://support.chasm.solutions/zumeproject" target="_blank">
+                <img class="center" src="<?php echo esc_attr( get_stylesheet_directory_uri() ) . '/assets/images/'; ?>zume-translation-infographic.png" alt="" />
+                </a>
+
 
                 <br>
                 <br>
                 <h3>Translation Progress</h3>
                 <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vS3rJ_shcus0nIrYWHzFpWY5F5UfiYWb4ql8kaNTtWDf5htAA8-KmiVwV49RYWIxZumfI6zVH5fY4ZG/pubhtml?gid=2023643321&amp;single=true&amp;widget=true&amp;headers=false"
-                width="50%" height="400px"
+                width="60%" height="400px"
                 ></iframe>
-<br>
+                <br>
                 <br><br>
                 <h3>Progress for each language</h3>
                 <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vS3rJ_shcus0nIrYWHzFpWY5F5UfiYWb4ql8kaNTtWDf5htAA8-KmiVwV49RYWIxZumfI6zVH5fY4ZG/pubhtml?gid=0&amp;single=true&amp;widget=true&amp;headers=false"
-                width="100%"
-                height="900px"
+                width="60%"
+                height="1000px"
                 ></iframe>
 
             </main>
