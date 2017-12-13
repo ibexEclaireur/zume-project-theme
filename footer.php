1c04cd2b82c48f38c58f958ab0d53b0f855531a7
Burning Sun <burntsun@users.noreply.github.com>
2017-12-13 10:46:27 +0100

    Add Facebook and Twitter icons in footer

--- footer.php
+++ footer.php
@@ -9,6 +9,8 @@
                             <p class="source-org copyright">
                                 &copy; <?php echo esc_html( date( 'Y' ) ); ?> <?php bloginfo( 'name' ); ?>.
                                 <?php if ( user_can( get_current_user_id(), 'manage_options' ) || current_user_can( 'coach' ) ) { echo '| <a href="/wp-admin" > Admin Panel</a>'; } ?>
+                                <a href="https://www.facebook.com/zumeproject" target="_blank" rel="noopener"><img src="<?php echo esc_attr( get_template_directory_uri() ); ?>/assets/images/facebook-square.svg" style="height: 23px" title="Facebook"></a>
+                                <a href="https://twitter.com/zumeproject" target="_blank" rel="noopener"><img src="<?php echo esc_attr( get_template_directory_uri() ); ?>/assets/images/twitter-square.svg" style="height: 23px" title="Twitter"></a>
                             </p>
                         </div>
                     </div> <!-- end #inner-footer -->
