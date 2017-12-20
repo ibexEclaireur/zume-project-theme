78358942d9c0e9ee969950618d75faa43972d62f
ibexEclaireur <34679199+ibexEclaireur@users.noreply.github.com>
2017-12-20 13:50:34 +0100

    reinserted deleted php

--- footer.php
+++ footer.php
@@ -1,20 +1,18 @@
 				<footer class="footer" role="contentinfo">
 					<div id="inner-footer" class="row" style="width:300px; float:left">
 						<div class="large-12 medium-12 columns">
-							<nav role="navigation">
-	    						<?php zume_joints_footer_links(); ?>
-                            </nav>
+							<nav role="navigation"></nav>
                         </div>
                         <div class="small-12 medium-8 columns">
+                            <?php if ( user_can( get_current_user_id(), 'manage_options' ) || current_user_can( 'coach' ) ) { echo '| <a href="/wp-admin" > Admin Panel</a>'; } ?>
                             <p class="source-org copyright">
-                                &copy; 2017 Zúme Project.
-                                
+                                &copy; <?php echo esc_html( date( 'Y' ) ); ?> <?php bloginfo( 'name' ); ?>.
                             </p>
                         </div>
                         <div class="small-12 medium-4 columns" style="float: left">
 							<span style="float: left">
-                                    <a href="https://www.facebook.com/zumeproject" target="_blank" rel="noopener"><img src="https://zumeproject.com/wp-content/themes/zume-project-theme/assets/images/facebook-square.svg" style="height: 23px" title="Facebook"></a>
-                                    <a href="https://twitter.com/zumeproject" target="_blank" rel="noopener"><img src="https://zumeproject.com/wp-content/themes/zume-project-theme/assets/images/twitter-square.svg" style="height: 23px" title="Twitter"></a>
+-                                    <a href="https://www.facebook.com/zumeproject" target="_blank" rel="noopener"><img src="<?php echo esc_attr( get_template_directory_uri() ); ?>/assets/images/facebook-square.svg" style="height: 23px" title="Facebook"></a>
+-                                    <a href="https://twitter.com/zumeproject" target="_blank" rel="noopener"><img src="<?php echo esc_attr( get_template_directory_uri() ); ?>/assets/images/twitter-square.svg" style="height: 23px" title="Twitter"></a>
                             </span>
                         </div>
                     </div> <!-- end #inner-footer -->
